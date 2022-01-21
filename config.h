566cae55e2c3432bd6c5546d5abe744468257523
ClosedGL2 <closedgl64@protonmail.com>
2022-01-20 23:34:57 -0700

    Config

--- config.h
+++ config.h
@@ -9,15 +9,15 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ffffff", "#222222" },
+	[SchemeNorm] = { "#ffffff", "#000000" },
 	[SchemeSel] = { "#000000", "#ffffff" },
 	[SchemeOut] = { "#000000", "#ffffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 25;
-static unsigned int min_lineheight = 25;
+static unsigned int lineheight = 21;
+static unsigned int min_lineheight = 21;
 
 /*
  * Characters not considered part of a word while deleting words
